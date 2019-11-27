b77b1e9d2d40647121593d38b1d4eabd0dd38b22
sjdavies2020 <stevenjdavies2013@gmail.com>
2019-11-27 10:31:37 -0500

    Survey to Json

--- public/js/apps.js
+++ public/js/apps.js
@@ -2,23 +2,26 @@ const urlLocation = window.location.href;
 let surveyList = null;
 let surveyOptions = null;
 let multiText = null;
+let qnum = 0;
+let qArray = [];
+let btn2 = document.getElementById("btn2");
+let btn1 = document.getElementById("btn1");
 
 if (urlLocation.includes("truefalse")) {
-	let btn1 = document.getElementById("btn1");
 	btn1.addEventListener("click", AddtoUl);
+	btn2.addEventListener("click", SaveTFToDB);
 	// Get the survey UL
 	surveyList = document.getElementById("surID");
 } else if (urlLocation.includes("multichoice")) {
 	let btn7 = document.getElementById("btn7");
 	btn7.addEventListener("click", AddtoUltxt);
 	let btn5 = document.getElementById("btn5");
-	btn5.addEventListener("click", AddtoUlmulti);
-	let btn6 = document.getElementById("btn6");
 	btn5.addEventListener("click", AddtoMultiOut);
+	let btn6 = document.getElementById("btn6");
+	btn6.addEventListener("click", SaveTFToDB);
 	// Get the survey UL
 	surveyList = document.getElementById("surID");
 	surveyOptions = document.getElementById("textElm");
-
 } else if (urlLocation.includes("textarea")) {
 	let btn3 = document.getElementById("btn3");
 	btn3.addEventListener("click", AddtoUltext);
@@ -32,6 +35,7 @@ function getResult(isNumber) {
 
 function AddtoUl(evt) {
 	evt.preventDefault();
+
 	var radios = document.getElementsByName("quest1");
 	for (var i = 0, length = radios.length; i < length; i++) {
 		if (radios[i].checked) {
@@ -50,6 +54,10 @@ function AddtoUl(evt) {
 	myLi.dataset.index = mySurveyName;
 	myLi.innerHTML = mySurveyName + "," + myQuestion + "," + myAnswers;
 	surveyList.appendChild(myLi);
+	qnum++;
+	var survey = new Survey(mySurveyName, qnum, myQuestion, myAnswers);
+	qArray.push(survey);
+	console.log(qArray);
 }
 
 function AddtoUltext(evt) {
@@ -61,6 +69,9 @@ function AddtoUltext(evt) {
 	myLi.dataset.index = mySurveyName;
 	myLi.innerHTML = mySurveyName + "," + myQuestion;
 	surveyList.appendChild(myLi);
+	var survey = new Survey(mySurveyName, qnum, myQuestion, "");
+	qArray.push(survey);
+	console.log(qArray);
 }
 
 function AddtoUlmulti(evt) {
@@ -83,7 +94,7 @@ function AddtoUltxt(evt) {
 		myButton.addEventListener("click", deleteMe);
 		myInput.className = "multiList";
 		myLi.id = "txt" + i;
-		myLi.className = "multiOut"
+		myLi.className = "multiOut";
 		myLi.appendChild(myInput);
 		myLi.appendChild(myButton);
 		surveyOptions.appendChild(myLi);
@@ -98,11 +109,10 @@ function deleteMe(evt) {
 
 function AddtoMultiOut(evt) {
 	evt.preventDefault();
-
 	surveyList = document.getElementById("surID");
 	let mySurveyName = document.getElementById("sName").value;
 	let myQuestion = document.querySelector("input#quest").value;
-	let myMultiOut = document.querySelectorAll('input.multiList');
+	let myMultiOut = document.querySelectorAll("input.multiList");
 	let myLi = document.createElement("li");
 	myLi.className = "list-group-item list-group-item-primary";
 	myLi.dataset.index = i;
@@ -110,5 +120,20 @@ function AddtoMultiOut(evt) {
 	for (let i = 0; i < myMultiOut.length; i++) {
 		myLi.innerHTML += "," + myMultiOut[i].value;
 	}
+	var survey = new Survey(mySurveyName, qnum, myQuestion, myLi.innerHTML);
+	qArray.push(survey);
+	console.log(qArray);
 	surveyList.appendChild(myLi);
+}
+
+function Survey(mySurveyName, qnum, myQuestion, myAnswers) {
+	this.qnumber = qnum;
+	this.name = mySurveyName;
+	this.question = myQuestion;
+	this.answer = myAnswers;
+}
+function SaveTFToDB(evt) {
+	evt.preventDefault();
+	let myJSON = JSON.stringify(qArray);
+	console.log(myJSON);
 }
